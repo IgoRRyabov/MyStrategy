9ca2519441d8462118fb7c5168711fe594cc7db9
IgorRyabov <i_ryabov@list.ru>
2024-05-29 20:02:18 +0300

    Первый коммит

---
+++ Source/MyStrategy/MyStrategy.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyStrategy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyStrategy, "MyStrategy" );
